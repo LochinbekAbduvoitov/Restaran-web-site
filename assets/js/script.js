f800236dd8239c4745c61a6d7c21cdfff617c394
Lochinbek  Abduvoitov <lochinbekabduvoitov8@gmail.com>
2022-07-07 18:06:24 +0500

    search content and menubar

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+"use strict"
+
+// Navbar toggle
+
+const navbar=document.querySelector("[data-navbar]");
+const navbarLinks=document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn=document.querySelector("[data-menu-toggle-btn]")
+
+menuToggleBtn.addEventListener("click",()=>{
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+});
+
+for(let i=0; i<navbarLinks.length; i++){
+    navbarLinks[i].addEventListener("click",()=>{
+        navbar.classList.toggle("active");
+        menuToggleBtn.classList.toggle("active");
+    })
+}
+
+// Header
+
+const header=document.querySelector("[data-header]");
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY >= 100){
+        header.classList.add("active");
+    }else{
+        header.classList.remove("active")
+    }
+})
+
+
+
+
+// Search box toggle
+
+const searchBtn=document.querySelector("[data-search-btn]");
+const searchContainer=document.querySelector("[data-search-container]");
+const searchSubmitBtn=document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn=document.querySelector("[data-search-close-btn]");
+
+const searchBoxElems=[searchBtn,searchSubmitBtn,searchCloseBtn];
+
+for(let i=0; i < searchBoxElems.length; i++){
+    searchBoxElems[i].addEventListener("click",()=>{
+        searchContainer.classList.toggle("active");
+        document.body.classList.toggle("active");
+    })
+}
